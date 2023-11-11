db3feafe007425c1e5f9c2cd11a3696d1bb34614
Marvelous <marvelous.udugbesi@icloud.com>
2023-11-11 16:31:46 +0000

    Add new quote

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const quotesContainer = [
     'The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt',
     "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
     'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success. -James Cameron',
-    'You may say I\'m a dreamer, but I\'m not the only one. I hope someday you\'ll join us. And the world will live as one. -John Lennon'
+    'You may say I\'m a dreamer, but I\'m not the only one. I hope someday you\'ll join us. And the world will live as one. -John Lennon',
+    'Is your father the Liz?, cause Agama eyes on you'
 ];
 
 function randQuotesGenerator() {
